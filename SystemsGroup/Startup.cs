ba81551ee5cca4d332fb4c03036fedcaf9858505
ANDREW WILKINSON <b5014553@hallam.shu.ac.uk>
2020-03-30 15:24:20 +0100

    Add project files.

---
+++ SystemsGroup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SystemsGroup.Startup))]
+namespace SystemsGroup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
