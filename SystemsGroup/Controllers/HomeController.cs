b5ec99eb022b7dfabb29d93bf95ecbf680eec2a3
DANIEL BODDY <danielboddy1@hotmail.co.uk>
2020-05-15 11:41:32 +0100

    css text black tables

--- SystemsGroup/Controllers/HomeController.cs
+++ SystemsGroup/Controllers/HomeController.cs
@@ -34,15 +34,8 @@ namespace SystemsGroup.Controllers
 
             return View();
         }
-        public ActionResult Admin()
-        {
-            string apiUri = Url.HttpRouteUrl("DefaultApi", new { controller = "admin", });
-            ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
 
-            return View();
-        }
 
-    }
         public ActionResult SendEmail()
         {
             return View();
